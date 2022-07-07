9e220a11f8c52f60637b300450c490a507b90bfe
radinaldhs <radinal@wit.id>
2022-07-07 23:34:27 +0700

    Banyak lah pokonya

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-$(window).scroll(function(){
+$(window).scroll(function () {
     var sticky = $('.sticky'),
         scroll = $(window).scrollTop();
-  
+
     if (scroll >= 100) sticky.addClass('fixed');
     else sticky.removeClass('fixed');
-  });
+});
 
 $('.owl-carousel').owlCarousel({
     items: 1,
@@ -32,26 +32,26 @@ function searchIcon() {
 var countDownDate = new Date("Oct 1, 2022 24:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="countdown"
-  document.getElementById("countdown").innerHTML = "<span class='countdown-time'>" + days + "</span>" + "d " + "<span class='countdown-time'>" + hours + "</span>" + "h " + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-  }
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="countdown"
+    document.getElementById("countdown").innerHTML = "<h3>Starting in:</h3><p style='margin-bottom: 0'><span class='countdown-time'>" + days + "</span>" + " d</p>" + "<hr>" + "<p style='margin-bottom: 0'><span class='countdown-time'>" + hours + "</span>" + " h</p>" + "<hr>" + "<p style='margin-bottom: 0'><span class='countdown-time'>" + minutes + "</span>" + " m</p>" + "<hr>" + "<p style='margin-bottom: 0'><span class='countdown-time'>" + seconds + "</span>" + " s</p>";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
 }, 1000);
